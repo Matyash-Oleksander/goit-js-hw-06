43230cbe46b1c2376bd5afc982276dd06687e6b1
Matyash-Oleksander <matyash19831010@gmail.com>
2022-08-24 10:06:57 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,6 +2,8 @@ const color = document.querySelector("body");
 const span = document.querySelector(".color");
 const btn = document.querySelector(".change-color");
 
+console.log(btn.type.value);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
